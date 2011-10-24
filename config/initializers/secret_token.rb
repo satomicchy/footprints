933dd0ead0b3a6fb0059c710b5465f172f614f46
satomicchy <satomi_t_sora@yahoo.co.jp>
2011-10-24 19:23:30 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footprints::Application.config.secret_token = '2e2862472c182a48155454505ce9c92508c9839282e4ba0e2824d8b8d1be7916d30b518347d4f278ab280301b823cef2dcd8f74f2ec77b5a1b88d8a70ead9844'
