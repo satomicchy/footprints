c87889c34f2b8a4ed7c4f6ac4811f1cdcf73b352
satomicchy <satomi_t_sora@yahoo.co.jp>
2011-11-05 23:48:33 +0900

    set friend submit field in application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :events
+  before_filter :events, :friends
 
   def events
     @event = Event.new
+  end
+
+  def friends
+    @friend = Friend.new
   end
 end
