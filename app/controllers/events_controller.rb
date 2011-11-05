d9832a92506f26eedcb55830f2dcc874a0b4df51
satomicchy <satomi_t_sora@yahoo.co.jp>
2011-11-06 01:23:41 +0900

    set friend list in Event#index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,7 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @events = Event.all
+    @friends = Friend.all
 
     respond_to do |format|
       format.html # index.html.erb
