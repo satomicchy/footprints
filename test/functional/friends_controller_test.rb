8da2ec3e79109e1a48dbc7e57ed2227c15cce812
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2011-12-03 09:14:44 +0900

    fix friends_controller_test
    delete friend action should redirect to /events page

--- test/functional/friends_controller_test.rb
+++ test/functional/friends_controller_test.rb
@@ -40,6 +40,6 @@ class FriendsControllerTest < ActionController::TestCase
       delete :destroy, id: @friend.to_param
     end
 
-    assert_redirected_to friends_path
+    assert_redirected_to events_path
   end
 end
