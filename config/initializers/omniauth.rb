64df1b423fc65260c467b6d43666ade4f0d28b85
satomicchy <satomi_t_sora@yahoo.co.jp>
2011-12-03 15:37:33 +0900

    prepare to deploy to heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  keys = YAML::load(File.open("config/twitter.yml"))
+  keys = if Rails.env.production?
+           {"CONSUMER_KEY" => ENV["TWITTER_CONSUMER_KEY"], "CONSUMER_SECRET" => ENV["TWITTER_CONSUMER_SECRET"]}
+         else
+           YAML::load(File.open("config/twitter.yml"))
+         end
   provider :twitter, keys["CONSUMER_KEY"], keys["CONSUMER_SECRET"]
 end
