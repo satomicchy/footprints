88e30f16d829f74896d0c00189a3c8a4b7f27edc
yalab <rudeboyjet@gmail.com>
2011-12-19 16:19:59 +0900

    Implement photos#create without Ajax.

--- test/functional/photos_controller_test.rb
+++ test/functional/photos_controller_test.rb
@@ -4,6 +4,7 @@ class PhotosControllerTest < ActionController::TestCase
   setup do
     @user = users(:one)
     login_as(@user)
+    stub_request(:post, "https://upload.twitter.com/1/statuses/update_with_media.json").to_return(:status => 200, :body => jsons(:update_with_media).to_json, :headers => {})
   end
 
   test "POST :create" do
