6e4c190e021e9b5b18eb6d4092b8808a9ae014ca
yalab <rudeboyjet@gmail.com>
2011-12-19 17:35:37 +0900

    Write users#map test.

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "map" do
+    get :map, :user_id => @user.to_param, :user => {:id => @user.to_param}
+    assert_equal 1, assigns(:events).values.flatten.length
+    assert_equal 1, assigns(:friends).values.flatten.length
+  end
 end
